57ecedd88ae2b270ede9be06a459eaa0c572e6f0
TZ <skybox2011@protonmail.com>
2025-12-30 18:04:02 -0500

    Add project files.

---
+++ Minecraft Logs Cleaner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Minecraft_Logs_Cleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // We need these in order to do anything, so get the directories first
+            string home_dir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string minecraft_dir = $"{home_dir}\\AppData\\Roaming\\.minecraft";
+
+            Console.WriteLine($"Home directory: {home_dir}");
+            // Check if the minecraft_dir exists
+            if (Directory.Exists(minecraft_dir))
+            {
+                Console.WriteLine($"Minecraft directory located: ({minecraft_dir})");
+            }
+            else
+            {
+                Console.WriteLine("Minecraft directory not found, exiting");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
